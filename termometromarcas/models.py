0ac360529201ec101ec7c9bbe4074580abb9662d
Matheus Aguiar <matheustxaguiar@gmail.com>
2022-11-03 12:09:00 -0300

    padrao observer

--- termometromarcas/models.py
+++ termometromarcas/models.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
+from random import choices
 from unittest.util import _MAX_LENGTH
 
 from django.db import models
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+status_usuario = [("ativo", "Ativo"), ("desativo", "Desativo")]
 
 
 # Create your models here.
@@ -50,15 +56,43 @@ class Usuario(models.Model):
     usuario = models.CharField('Usuario', max_length=30, null=False, blank=False)
     email = models.CharField('Email', max_length=50, null=False, blank=False)
     senha = models.CharField('Senha', max_length=30, null=False, blank=False)
-    pesquisas = models.ManyToManyField(Pesquisa, null=True, blank=True)
+    pesquisas = models.ManyToManyField(Pesquisa, blank=True)
 
     def __str__(self):
         return self.usuario
 
-    
+
 class Fisico(Usuario):
     cpf = models.IntegerField('CPF', null=False, blank=False)
+    status = models.CharField(max_length=255, choices=status_usuario, default="ativo")
+    observadores = []
 
+    def __str__(self):
+        return self.cpf
+    
+    def adicionarObservador(self, observador):
+        self.observadores.append(observador)
+
+
+# class Observador():
+#     def update()
+
+class UsuarioObserver():
+    def update(self, instancia):
+        print("teste")
+        if instancia.status=='desativo':
+            print("usuario desativado")
+
+            
+@receiver(pre_save, sender=Fisico)
+def notifyObservers(sender, instance, **kwargs):
+    listaObservadores=[]
+    ob1 = UsuarioObserver()
+    listaObservadores.append(ob1)
+    for item in listaObservadores:
+        item.update(instance)
+    
 
 class Juridico(Usuario):
     cnpj = models.BigIntegerField('CPNJ', null=False, blank=False)
+

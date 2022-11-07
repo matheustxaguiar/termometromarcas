6651e041be08e4ff3d7461d3c61458599354b21d
Matheus Aguiar <matheustxaguiar@gmail.com>
2022-11-07 13:23:54 -0300

    validation fields

--- termometromarcas/Builder.py
+++ termometromarcas/Builder.py
@@ -1,6 +1,7 @@
 # pylint: disable=too-few-public-methods
 # pylint: disable=arguments-differ
-"Builder Concept Sample Code"
+import re
+from rest_framework import serializers
 from abc import ABCMeta, abstractmethod
 
 from .models.Usuario import Usuario
@@ -34,20 +35,53 @@ class UsuarioBuilder(IBuilder):
     # "The Concrete Builder."
 
     def validar_usuario(self, usuario):
-        print(usuario)
-        return self
+        regex = r'\b[A-Za-z0-9 ]{2,}\b'
+        if(re.fullmatch(regex, usuario)):
+            return usuario
+ 
+        else:
+            raise serializers.ValidationError("O usuário não pode ter caracteres especiais")
+       
 
     def validar_email(self, email):
-        print(email)
-        return self
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if(re.fullmatch(regex, email)):
+            return email
+ 
+        else:
+            raise serializers.ValidationError("Email invalido")
 
     def validar_senha(self, senha):
-        print(senha)
+        l, u, p, d = 0, 0, 0, 0
+        s = senha
+        if (len(s) >= 8):
+            for i in s:
+        
+                # counting lowercase alphabets
+                if (i.islower()):
+                    l+=1           
+        
+                # counting uppercase alphabets
+                if (i.isupper()):
+                    u+=1           
+        
+                # counting digits
+                if (i.isdigit()):
+                    d+=1           
+        
+                # counting the mentioned special characters
+                if(i=='@'or i=='$' or i=='_'):
+                    p+=1          
+        if (l>=1 and u>=1 and p>=1 and d>=1 and l+p+u+d==len(s)):
+            print("Valid Password")
+        else:
+            raise serializers.ValidationError("a senha deve conter no mínimo 8 caracteres, sendo eles: números, letras maiúsculas, minúsculas e caracteres especiais")
+
         return self
 
     def get_result(self):
-        print("mds q sofrimento")
-
+        # ???
+        return self
 
 class Director():
     # "The Director, building a complex representation."
